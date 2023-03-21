b78980f98c835970dd18f36b87a9b15807026c36
atdilakshan <dilakshan0501@gmail.com>
2023-03-21 15:27:17 +0530

    initial commit

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+using gRPCClient;
+
+// create channel
+GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:5198");
+
+// create client
+//var client = new Greeter.GreeterClient(channel);
+
+//HelloRequest request = new HelloRequest() { Name = "Dilakshan" };
+//HelloReply response = client.SayHello(request);
+
+//Console.WriteLine(response.Message);
+
+
+// create client
+var client = new StockServer.StockServerClient(channel);
+
+
+// method 1
+ValueByNameRequest request = new ValueByNameRequest() { StockName = "Test1" };
+StockResult response = client.GetValueByName(request);
+
+Console.WriteLine(string.Format("Stock {0} value is {1}", response.Name, response.Value));
+
+// method 2
+//var reply = client.GetAllStockValues(new Empty());
+
+//foreach (var stResult in reply.Result)
+//{
+
+//    Console.WriteLine(string.Format("Stock {0} value is {1}", stResult.Name, stResult.Value));
+//}
+
+// method 3
+//ValueByNameRequest streamRequest = new ValueByNameRequest() { StockName = "Test2" };
+//var response1 = client.GetStockValue(streamRequest);
+
+//while (await response1.ResponseStream.MoveNext())
+//{
+//    Console.WriteLine(string.Format("Stock {0} value is {1} on {2}",
+//        response1.ResponseStream.Current.Name,
+//        response1.ResponseStream.Current.Value,
+//        DateTime.Now.ToString()));
+//}
+
+Console.WriteLine("gRPC Call Completed!!!");
+
+// call the service
+Console.ReadLine();
---
+++ gRPCServer/Services/StockService.cs
@@ -0,0 +1,66 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using static gRPCServer.StockServer;
+
+namespace gRPCServer.Services
+{
+    public class StockService : StockServerBase
+    {
+        public Dictionary<string, int> Source = new Dictionary<string, int>()
+        {
+            {"Test1", 890 },
+            {"Test2", 100 },
+            {"Test3", 90 },
+            {"Test4", 45 },
+            {"Test5", 897 }
+        };
+        public override Task<StockResult> GetValueByName(ValueByNameRequest request, ServerCallContext context)
+        {
+            StockResult response = new StockResult
+            {
+                Value = Source[request.StockName],
+                Name = request.StockName
+            };
+
+
+            return Task.FromResult(response);
+        }
+
+        public override Task<StockResponses> GetAllStockValues(
+            Empty request, ServerCallContext context)
+        {
+            StockResponses response = new StockResponses();
+
+            foreach (var key in Source)
+            {
+                response.Result.Add(new StockResult()
+                {
+                    Value = key.Value,
+                    Name = key.Key
+                }
+                    );
+            }
+
+            return Task.FromResult(response);
+        }
+        public override async Task GetStockValue(
+            ValueByNameRequest request,
+            IServerStreamWriter<StockResult> responseStream,
+            ServerCallContext context)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                await responseStream.WriteAsync(
+                    new StockResult
+                    {
+                        Value = Source[request.StockName] ,
+                        Name = request.StockName
+                    }
+                    );
+                await Task.Delay(1000);
+        }
+    }
+}
+}
